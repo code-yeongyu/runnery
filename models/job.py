98f9686ba5483382860031f0c10042155d0811f2
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-11-03 15:42:32 +0900

    Rename `image` -> `runs_on`

--- models/job.py
+++ models/job.py
@@ -7,6 +7,6 @@ from .step import Step
 
 class Job(BaseModel):
     name: Optional[str] = None
-    image: Optional[str] = None
+    runs_on: Optional[str] = 'local'
     steps: list[Step]
     envs: Optional[dict[str, str]] = None
