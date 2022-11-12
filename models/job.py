ad785e3f4fd48c062b61dcc27cb5f0755911bdcb
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-11-12 21:38:25 +0900

    Remove field `runs_on` from `Job`

--- models/job.py
+++ models/job.py
@@ -1,27 +1,14 @@
 from typing import Optional
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel
 
 from .step import Step
 
 
 class Job(BaseModel):
     name: Optional[str] = None
-    runs_on: Optional[str] = 'local'
     steps: list[Step]
     envs: Optional[dict[str, str]] = {}
 
     class Config: # pylint: disable=too-few-public-methods
         frozen = True
-
-    @validator('runs_on')
-    @classmethod
-    def validate_runs_on(cls, runs_on: str) -> str:
-        runs_on = runs_on or 'local'
-        if runs_on == 'local':
-            return runs_on
-
-        if runs_on.startswith('docker[') and runs_on.endswith(']'):
-            return runs_on
-
-        raise ValueError('`runs_on` must be in format `docker[python:3.9]` or `docker[python:3.9-alpine]` or `local`')
--- models/test/test_job.py
+++ models/test/test_job.py
@@ -1,25 +1,8 @@
-from typing import Optional
+from models import Job
 
-import pytest
+
 from models import Job
 
 
 class TestJob: # pylint: disable=too-few-public-methods
-    class TestRunsOnValidator:
-        @pytest.mark.parametrize(
-            'allowed_value', ['local', 'docker[python:3.9]', 'docker[python:3.9-alpine]', '', None]
-        )
-        def test_allow_formatted_text(self, allowed_value: Optional[str]) -> None:
-            # when
-            job = Job(runs_on=allowed_value, steps = [])
-
-            # then
-            assert job.runs_on == allowed_value if allowed_value else 'local'
-
-        @pytest.mark.parametrize(
-            'runs_on', ['loc', 'docker[python:3.9', 'doc', 'docker[python:3.9-alpine', 'wrong', 'nothing']
-        )
-        def test_not_allow_wrong_text(self, runs_on: str) -> None:
-            # when & then
-            with pytest.raises(ValueError):
-                Job(runs_on=runs_on, steps = [])
+    pass
